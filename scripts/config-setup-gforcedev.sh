d7875ef10933bc34e5b83496d3605846c7abfcda
Gforcedev <gforcedev@users.noreply.github.com>
2021-03-05 08:48:28 +0000

    Update config-setup-gforcedev.sh

--- scripts/config-setup-gforcedev.sh
+++ scripts/config-setup-gforcedev.sh
@@ -4,5 +4,6 @@ echo ".cfg" >> .gitignore
 git clone git@github.com:gforcedev/dotfiles.git $HOME/.cfg --bare
 
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 echo "Setup complete - run 'config checkout' to instantiate the files"
