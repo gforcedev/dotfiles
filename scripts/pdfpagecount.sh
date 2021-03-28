b7abb2620a0a002018c22911b078bffa2db9a3ec
Gforcedev <tecshotclark@gmail.com>
2021-03-28 15:34:55 +0100

    Add descriptive comment

--- scripts/pdfpagecount.sh
+++ scripts/pdfpagecount.sh
@@ -1,1 +1,2 @@
+# Sum the pagecount of all PDFs in this directory and recursively in subdirectories
 find . -name \*.pdf -exec pdfinfo {} \; | grep Pages | sed -e "s/Pages:\s*//g" | awk '{ sum += $1;} END { print sum; }'
